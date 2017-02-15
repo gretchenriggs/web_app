51c07f001a7f8d3d9c1a10a1d708bce4ad19aa68
gretchenriggs <gretchen.m.riggs@gmail.com>
2017-02-14 17:07:31 -0700

    Updated all pages

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
+@app.route('/about')
+def contact():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8105, debug=True)
